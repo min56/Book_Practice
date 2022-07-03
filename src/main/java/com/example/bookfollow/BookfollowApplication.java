21bb830fd7bbb2b204fa0e7d1256ce2f51b201d7
min <minchae571@gmail.com>
2022-07-03 15:18:21 +0900

    Initial commit

---
+++ src/main/java/com/example/bookfollow/BookfollowApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookfollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookfollowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookfollowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookfollow/BookfollowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookfollow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookfollowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
